6b962be7909362335e0463f0358ae30023f0c715
Adrian Deardorff <80deardorff@live.com>
2018-06-12 00:48:33 -0700

    finished in class exercises

---
+++ js/main.js
@@ -0,0 +1,96 @@
+var testOne = "What kind of variable am I?";
+// string
+
+var testTwo = 256;
+// number
+
+var testThree = false;
+// boolean
+
+var testFour = 'true';
+// string
+
+var testFive = '128';
+// string
+
+var testSix = "";
+// string
+
+// ---------------------------------------------
+
+var testOne = "hello" + "there";
+// "hellothere" string
+
+var testTwo = '100' + 28;
+// '10028' string
+
+var testThree = true + 3;
+// 4 number
+
+var testFour = 'false' + 5;
+// 'false5' string
+
+var testFive = '128' - 3;
+// 125 number
+
+var testSix = "" * 9;
+// 0 number
+
+var testSeven = 'zero' - 1;
+// NaN not a number
+
+var testEight = 'five' .length;
+// 4 number
+
+var testNine = 'five'[0] +'ever';
+// 'fever' string
+
+var testTen = 'a' > 'b';
+// false boolean
+
+var testEleven = 'number' .length < 7;
+// true boolean
+
+var testTwelve = '12' == 12;
+// true boolean
+
+var testThirteen = 'twelve' == 12;
+// false boolean
+
+var testFourteen = '14' === 14;
+// false boolean
+
+var testFifteen = 'Hi there'[0] + 'open sesame'[2] + 'alohamora'[1] + 'eulalia'[4] + 'harkonnen'[4] + 'BATMAN' [6];
+// 'Helloundefined' string
+
+// ---------------------------------------------
+
+7. "Hello, my name is Kingsley Pappagiorgio."
+
+8. 120
+
+9. '1010ten'
+
+10. 'Num4 is greater than 1'
+
+11. 'Else if statement ran'
+
+12. 'Else if statement ran'
+
+13. 16
+
+14. 6
+
+15. 0 "o"; 2 "o"; 6 "o"; 8 "o"
+
+16. "o"; "u"; "e"; "o"; "a"; "e"; "e"; "a"; "i"; "e"; "o"; "a";
+
+17. "a is NOT greater than 25 or b is NOT less than 100";
+    "a is greater than 25 and b is less than 100";
+    "a is greater than 25 and b is less than 100"
+
+18. "Neither if nor else if statement ran";
+    "c is equal to 25";
+    "d is less than or equal to 100";
+
+19. "Syntax Error Line 11";
